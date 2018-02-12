40938e1ba4e729da51fe54b9f6638cb76f57e536
Rohan Kalgutkar <rohankalgutkar@Rohans-MacBook-Air.local>
2018-02-12 11:31:38 +0530

    Updated coming-soon files

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const path = require('path');
 const port = process.env.PORT || 3001;
 var app = express();
 
-// Coming soon static html is set @ path /
-app.use('/', express.static(__dirname + '/public/'));
+// Loading coming soon static assets
+app.use(express.static(path.join(__dirname, 'public')));
 
-//
+//app.use('/', express.static(__dirname + '/public/'));
+
 hbs.registerPartials(__dirname + '/views/partials/');
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 })
 app.set('view engine', 'hbs');
 
+// For logging / debugging
 // app.use((req, res, next) => {
 //   var now = new Date().toString();
 //   var log = `${now}: ${req.method} ${req.url}`;
@@ -21,6 +24,10 @@ app.set('view engine', 'hbs');
 //     console.log('Unable to write to log file');
 //   });
 // });
+
+app.get('/', (req, res) => {
+  res.render('coming-soon/index.hbs');
+});
 
 app.get('/dev', (req, res) => {
   res.render('index.hbs', {
